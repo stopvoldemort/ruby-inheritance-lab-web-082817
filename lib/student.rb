bde6c2a9e50b275650aad2475fcbd1d8393d3b57
stopvoldemort <david.squires1@gmail.com>
2017-09-01 12:33:01 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
 
+  def initialize(first_name=nil, last_name=nil)
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
